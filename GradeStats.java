5a2553fd71da72e45d69fa222aab4972cd4c81ac
Kathy-Zeng <70964239+Kathy-Zeng@users.noreply.github.com>
2021-01-23 20:17:52 -0800

    Update GradeStats.java

--- GradeStats.java
+++ GradeStats.java
@@ -7,11 +7,15 @@ import java.util.Scanner;
 
 public class GradeStats
 {
-	private int[] score;
+	private int[] score; // Identify students' scores properties
+	private String input; // Get a user input in getInput() method and in
+	// calculateIt() method in a do-while condition, a user can stop a loop
+	// by typing "Quit". 
 	
 	public GradeStats()
 	{
-		score = new int[20]{2, 4, 6, 7, 8};
+		score = new int[20];
+		input = new String("");
 	}
 
 	public static void main(String[] args)
@@ -20,41 +24,55 @@ public class GradeStats
 		gs.calculateIt();
 	}
 
-	// A new String variable
+	// Call all printing and calculating methods.
 	public void calculateIt()
 	{
-		String input = new String("");
-		do
-		{
-			getInput();
-		} while(!input.equalsIgnoreCase("Quit") );
-		max();
-		minNum();
-		averageNum();
+		getInput();
+		int numScoreBelow75 = gScore();
+		int max = max(); 
+		int minNum = minNum();
+		double averageNum = averageNum();
 		orderNum();
-		medianNum();
-		computeScores();
-	}
-
-	//
-	public void getInput()
-	{
-		String input = new String("");
-		Scanner src = new Scanner(System.in);
-		System.out.println("\n\n\n");
-		if(!input.equalsIgnoreCase("Quit") )
-		{
-			System.out.print("Type in the score. Type " + "Quit" + " to end the program --> ");
-			input = Integer.parseInt(src.next() );
-		}
+		int[] orderScore = new int[20];
+		double medianNum = medianNum(orderScore);
+		computeScores(numScoreBelow75, max, minNum, averageNum, medianNum);
 	}
 	
-	// Compare values to each other. 
+	// Print 3 blank lines and get a user input in a do-while loop.
+	public void getInput() 
+	{
+		Scanner src = new Scanner(System.in);
+		System.out.println("\n\n\n");
+		int index = 0;
+		do
+		{
+			System.out.print("Type in the score. Type " + "Quit" + 
+				" to end the program --> "); 
+			input = src.next();
+			if(!input.equalsIgnoreCase("Quit") )
+			{	
+				score[index] = Integer.parseInt(input);
+				index++; 
+			}	
+		} while(!input.equalsIgnoreCase("Quit") );
+	} 
+	
+	// Determine whether any students' score below 75% by using an if statement. 
+	public int gScore()
+	{
+		int numStudent = 0; 
+		for(int i = 0; i < score.length; i++)
+			if(score[i] < 75)
+				numStudent += 1;
+			return numStudent;
+	}
+	
+	// Compare values to each other until it finds a biggest score number. 
 	public int max()
 	{
 		int maximum = -1;
 		for(int i = 0; i < score.length; i++)
-        { 
+        	{ 
 			if(score[i] > maximum)
 			{
 				maximum = score[i];
@@ -63,29 +81,30 @@ public class GradeStats
 		return maximum;
 	}
 	
-	//
+	// Compare values to each other until it finds a smallest score number. 
 	public int minNum()
 	{
 		int minimum = -1;  
-        for(int j = 0; j < score.length; j++)   
-        {  
-                if(score[j] < minimum)   
-                {  
-                    minimum = score[j];
-                }    
-        }  
-        return minimum;  
+        	for(int j = 0; j < score.length; j++)   
+        	{  
+             	      if(score[j] < minimum)   
+            	      {  
+                           minimum = score[j];
+           	      }    
+       	        }  
+        	return minimum;  
 	}
 	
-	// If even. If odd.
-	public void averageNum()
+	// Calculates an average score.
+	public double averageNum()
 	{
-		double[] arr;
-		for(int i = 0; i < arr.length; i++)
+		int total = 0;
+		for(int i = 0; i < score.length; i++)
 		{
-        	total = total + arr[i];
-        }
-        double average = total / arr.length;
+			total += score[i]; 
+		}
+        	double average = total / score.length;
+		return average;
 	}
 	
 	// Organizes scores from least to greatest.
@@ -94,7 +113,7 @@ public class GradeStats
 		int num1 = 0; 
 		int num2 = 0; 
 		int tempVan = 0;
-		for(int i = 0; i < score.length(); i++)
+		for(int i = 0; i < score.length; i++)
 		{
 			if(num1 < num2)
 			{	
@@ -105,60 +124,47 @@ public class GradeStats
 		}
 	}
 	
-	//
-	public void medianNum()
+	// Calculates median scores of students. If even, it calculates average of the 
+	// two middle numbers. If odd, just a middle number.
+	public double medianNum(int[] orderScore) 
 	{
 		double median = 0.0; 
-		double avg = 0.0;
-		int total = 0; 
-		int index = 0;
-		if(score % 1 == 0)
-		{
-			for (int i = 0; i < score.length(); i++)
-			{
-				sum += score[20];
-			}
-			total = sum;
-			avg = total / index;
-
-			median = score[score.length/2];
-		}	
-	
-		else if(score % 2 == 0)
-		{
-			int sum = 0;
-			for (int j = 0; j < score.length; j++)
-			{
-				sum += score[j];
-			}
-		}
+		if(orderScore.length % 2 != 0)
+			median = (orderScore[score.length/2] + orderScore[score.length/2])/2;	
+		else
+			median = (orderScore[orderScore.length/2] + orderScore[score.length/2 - 1])/2;
+		return median;
 	}
-	
+
 	// Prints overall results of students.
-	public void computeScores()
+	public void computeScores(int numStudentBelow75, int maxNum, int min, double
+	avg, double med)
 	{
-		int num = 0;
-		int minimum = 0;
-		int maximum = 0;
-		double average = 0.0;
-		double median = 0.0;
 		System.out.println("\n");
 		System.out.print("Here is the data you entered: ");
-		for(int i = 0; i < score.length; i++)
+		int i;
+		for(i = 0; i < score.length; i++)
 		{
-			System.out.printf("%nStudent %d's score: ", score[i]);
-			int students = 1;
-			if(score < 75)
-				System.out.println("\n");
-				System.out.print("There were " + students + "who scored below 75%: "
-					+ students + ".");
+			if(numStudentBelow75 < 10)
+				System.out.printf("%n%19s", "Student %d's score: %4d", i+1, score[i]);
+			else if(numStudentBelow75 >= 10)
+				System.out.printf("%n%20s", "Student %d's score: %4d", i+1, score[i]);
 		}
+		System.out.printf("There were %d students who scored below 75%% = %4d", 
+			numStudentBelow75, score.length);
+		for(int j = 0; j < score.length - 1; j++)
+		{
+			if(score[j] < 75)
+				System.out.printf("Student %d,", j+1);
+		}
+		if(score[score.length - 1] < 75) 
+			System.out.printf("Student %d.", score.length); 
 		System.out.println("\n");
-		System.out.printf("Number of scores: %4d", score);
-		System.out.printf("%nMinimum: %14d", minimum);
-		System.out.printf("%nMaximum: %14d", maximum);
-		System.out.printf("%nAverage: %14.1f", average);
-		System.out.printf("%nMedian: %14.1f", median);
+		System.out.printf("%n%17s: %4d", "Number of scores ", score.length);
+		System.out.printf("%n%17s: %4d", "Minimum ", min);
+		System.out.printf("%n%17s: %4d", "Maximum ", maxNum);
+		System.out.printf("%n%17s: %4.1f", "Average ", avg);
+		System.out.printf("%n%17s: %4.1f", "Median", med);
 		System.out.println("\n\n\n");
 	}
 }
